702d2f92045a556feeb59b05bea39454e11308d5
WonJun Lee <dldnjs1013@nate.com>
2022-10-10 02:31:44 +0900

    Fix: add offset

--- gitdot-cli/Program.fs
+++ gitdot-cli/Program.fs
@@ -2,16 +2,23 @@
 open LibGit2Sharp.Handlers
 open System
 open System.IO
+open System.Globalization
 
 // args
 let args = System.Environment.GetCommandLineArgs()
-let (repoPath, gitdotFilePath, author, email) = (args.[1], args.[2], args.[3], args.[3])
+let (repoPath, gitdotFilePath, author, email) = (args.[1], args.[2], args.[3], args.[4])
+let offset = Int32.Parse(args |> Array.tryItem 5 |> Option.defaultValue "1") // In korea, we need to add 1 day.. I don't know why...
 let commitFilePath= repoPath + "\\gitdot.txt" 
 
 // init git repo
 File.Create(commitFilePath).Close()
 LibGit2Sharp.Repository.Init(repoPath)  |> ignore
 let repo = new Repository(repoPath)
+
+// print args
+printfn "Repository : %s" repoPath
+printfn "git-dot file : %s" gitdotFilePath
+printfn "author : %s / email :%s" author email
 
 // read data
 type gitDot = { date: DateTime; commitCount : int }
@@ -26,7 +33,7 @@ let gitDots =
 let commitsDates = 
     gitDots |> Array.toList
     |> List.collect (fun gitDot -> 
-        let date = gitDot.date
+        let date = gitDot.date.AddDays(offset )
         let commitCount = gitDot.commitCount
         let dates = List.init commitCount (fun _ -> date)
         dates)
